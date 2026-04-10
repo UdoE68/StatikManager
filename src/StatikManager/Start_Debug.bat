c1d4aaeef976fb2b38c97ee3c5fcb9c98226e9a4
Dein Name <deine@email.de>
2026-04-10 22:41:52 +0200

    fix(Start_Debug.bat): Pfad auf V2 korrigiert

--- src/StatikManager/Start_Debug.bat
+++ src/StatikManager/Start_Debug.bat
@@ -1,5 +1,5 @@
 @echo off
-set EXE=C:\KI\StatikManager_V1\src\StatikManager\bin\x64\Debug\net48\StatikManager.exe
+set EXE=C:\KI\StatikManager_V2\src\StatikManager\bin\x64\Debug\net48\StatikManager.exe
 if not exist "%EXE%" (
     echo FEHLER: Debug-EXE nicht gefunden: %EXE%
     echo Bitte zuerst einen Debug-Build durchfuehren.
